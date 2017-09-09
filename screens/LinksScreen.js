d98198a000085e239c79320b7f904facb4d2b9d0
Cortland Perry <Cortland@huntsman-ve702-3739.apng.wlan.private.upenn.edu>
2017-09-09 11:42:29 -0400

    changes, made a button

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAaU9WGnCiUtXK5piych-okQngLUxSjBqI",
+  authDomain: "curfew-81f8f.firebaseapp.com",
+  databaseURL: "https://curfew-81f8f.firebaseio.com/",
+  storageBucket: "curfew-81f8f.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
+
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     title: 'Links',
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import { AppRegistry, Text, View, Button, Alert, StyleSheet} from 'react-native';
+
+
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'test title',
+    title: 'Child',
   };
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+
+
+
+    return (
+
+    	<View style = {styles.container}>
+        <View style = {styles.buttonContainer}>
+
+    		
+
+	    	  <Button
+  			 onPress={() => { Alert.alert('You Are Now Home')}}
+  			 title="Check In!"
+         color = "white"
+  			 />
+        </View>
+	    </View>
+
+    );
+
   }
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  buttonContainer: {
+    backgroundColor: '#2E9298',
+    borderRadius: 25,
+    padding: 10,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 3,
+      height: 3
+    },
+    shadowRadius: 25,
+    shadowOpacity: 0.25
+  }
+})
