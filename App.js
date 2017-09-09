71510461ac9b805655932b7a9db8f2da91ac5553
Emily Cohen <emilycohen@huntsman-ve702-3352.apng.wlan.private.upenn.edu>
2017-09-09 11:41:33 -0400

    Emily's firebase commit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
 
+
 export default class App extends React.Component {
+
   state = {
     assetsAreLoaded: false,
   };
